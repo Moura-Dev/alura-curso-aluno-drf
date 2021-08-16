e78787406390ab54eb0868a0cf7d1e0f048c44a8
moura-dev <junior.moura19@hotmail.com>
2021-08-16 20:24:43 -0300

    unzip

---
+++ escola/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Aluno(models.Model):
+    nome = models.CharField(max_length=30, null=True)
+    rg = models.CharField(max_length=9, null=True)
+    cpf = models.CharField(max_length=11, null=True)
+    data_nascimento = models.DateField()
+
+    def __str__(self):
+        return self.nome
+
+class Curso(models.Model):
+    NIVEL = (
+        ("B", 'Basico',),
+        ("I", 'Intermediario'),
+        ('A', 'Avançado')
+    )
+    codigo_curso = models.CharField(max_length=10, null=True)
+    descricao = models.CharField(max_length=100, null=True)
+    nivel = models.CharField(max_length=1, choices=NIVEL, blank=False, null=False, default='B')
+
+    def __str__(self):
+        return self.descricao
+
+class Matricula(models.Model):
+    PERIODO = (
+        ("M", 'Matutino',),
+        ("V", 'Vespertino'),
+        ('N', 'Noturno')
+    )
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    periodo = models.CharField(max_length=1, choices=PERIODO, blank=False, null=False, default='M')
